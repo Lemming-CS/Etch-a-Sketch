37249008225d8ec01e7b48b999852445ad9be100
Lemming-CS <220599209+Lemming-CS@users.noreply.github.com>
2025-12-11 20:09:46 +0600

    Add a rainbow toggle and base case grid generation

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let grid = document.querySelector(".grid");
 let submit = document.querySelector(".submit");
+let rainbow = document.querySelector(".rainbow");
+let israinbow = false;
 
-submit.addEventListener("click", function(event) {
-    event.preventDefault();
+makeGrid()
+function makeGrid()
+{
     let number = Number(document.querySelector(".number").value);
+    if (!number) {
+        number = 30;
+    }
     if ((number > 0 && number <= 100) && Number.isInteger(number))
     {
         grid.replaceChildren()
@@ -11,17 +17,34 @@ submit.addEventListener("click", function(event) {
         {
             let column = document.createElement("div");
             column.classList.add("column");
-            grid.appendChild(column)
+            grid.appendChild(column);
             for(let j = 0; j < number; j++)
             {
                 let cell = document.createElement("div");
                 cell.classList.add("cell");
-                cell.onmouseover = () => { cell.style.background = "black"; };
-                column.appendChild(cell)
+                if (israinbow)
+                {
+                    cell.onmouseover = () => { cell.style.background = `rgb(${Math.floor(Math.random()*255)},
+                     ${Math.floor(Math.random()*255)},
+                     ${Math.floor(Math.random()*255)}`; };
+                }
+                else
+                {
+                    cell.onmouseover = () => { cell.style.background = "black"; };
+                }
+                column.appendChild(cell);
             }
         }
     }
     else {
-        alert("Number must be a positive integer below 100")
+        alert("Number must be a positive integer below 100");
     }
-})
+}
+
+rainbow.addEventListener("click", function() {
+    israinbow = !israinbow
+    rainbow.classList.toggle("act")
+    makeGrid()
+});
+
+submit.addEventListener("click", makeGrid);
